c87a2bdaa624f6ca9205a783b5bd64e2946199fa
绪锋 <910408228@qq.com>
2019-08-08 16:47:49 +0800

    绑定数据

--- src/todolist/TodoList.js
+++ src/todolist/TodoList.js
@@ -1,16 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class TodoList extends Component {
-  state = {  }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+
+    this.WatchInput = this.WatchInput.bind(this)
+    this.SaveData = this.SaveData.bind(this)
+  }
+
   render() { 
     return (
-      <div>
+      <Fragment>
         <div>
-          <input type="text"/>
-          <button>增加</button>
+          <input
+            type="text"
+            onChange={this.WatchInput}
+            // 将自身变成一个参数，传递给 this.inputA,所以能在下面找到那个 this.inputA 属性
+            ref={(input)=>{this.inputA=input}}
+            value={this.state.inputValue}
+          />
+          <button onClick={this.SaveData}>增加</button>
         </div>
-      </div>
+        {/* 
+          ==== 列表渲染 ====
+          表达式必须放在花括号内
+        */}
+        {
+           this.state.list.map((item, index) => {
+            return (
+              <div key={index+item}>{item}</div>
+            )
+          })
+        }
+        {/* <div>{this.state.inputValue}</div> */}
+      </Fragment>
     )
+  }
+
+  // 监听输入框的值
+  WatchInput () {
+    this.setState({
+      inputValue: this.inputA.value
+    })
+  }
+
+  // 保存数据
+  SaveData () {
+    this.setState({
+      list: [
+        ...this.state.list,
+        this.state.inputValue
+      ],
+      inputValue: ''
+    })
   }
 }
  
