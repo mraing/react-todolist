d7abeda217be335f01036b2832ebe9cd0a0bc03a
绪锋 <910408228@qq.com>
2019-08-08 15:48:30 +0800

    init react project

---
+++ src/todolist/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+  state = {  }
+  render() { 
+    return (
+      <div>
+        <div>
+          <input type="text"/>
+          <button>增加</button>
+        </div>
+      </div>
+    )
+  }
+}
+ 
+export default TodoList
